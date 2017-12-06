027ba59aa6bfe71b0f66524d9a63f84fc8ff8b42
jasongao97 <jasongao97@gmail.com>
2017-12-06 16:39:32 +0800

    change path

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
 <template>
   <div id="app">
+    <h1 class="title">On Reflection</h1>
+    <h2 class="subTitle">A project made by
+      <span v-if="!ifShowInfo" @click="changeMsg">{{ msg }}</span>
+      <span v-else>
+        <a href="https://www.instagram.com/gosgjkaj_official/" target="_blank">@gosgjkaj_offical</a>
+        <a href="https://www.instagram.com/jasongao97/" target="_blank">@jasongao97</a>
+      </span>
+    </h2>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      msg: 'Cornography',
+      ifShowInfo: false
+    }
+  },
+  methods: {
+    changeMsg: function () {
+      this.ifShowInfo = true
+    }
+  }
 }
 </script>
 
 <style>
+@import "./assets/display.css";
+
 #app {
   font-family: 'roboto condensed', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  display: flex;
-  justify-content: center;
+  justify-content: left;
   color: #2c3e50;
   margin: 40px;
+}
+
+h1, h2 {
+  margin: 0;
+  padding: 0;
+}
+
+.title {
+  font-size: 46px;
+}
+
+.subTitle {
+  font-family: 'roboto';
+  margin-top: 10px;
+  font-size: 18px;
+  font-weight: 300;
+  line-height: 32px;
+}
+
+.subTitle span {
+  text-decoration: underline;
+}
+
+a {
+  color: #2c3e50;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="hello">
-    <h1 class="title">On Reflection</h1>
-    <h2 class="subTitle">A project made by
-      <span v-if="!ifShowInfo" @click="changeMsg">{{ msg }}</span>
-      <span v-else>
-        <a href="https://www.instagram.com/gosgjkaj_official/" target="_blank">@gosgjkaj_offical</a>
-        &
-        <a href="https://www.instagram.com/jasongao97/" target="_blank">@jasongao97</a>
-      </span>
-    </h2>
     <!-- <canvas id="gradientBlock" width="320" height="320"></canvas> -->
-    <div class="imagesBlock" id="wrapper">
+    <div class="imagesBlock hidden-xs-only" id="wrapper">
       <img v-for="image in images" :key="image.id" :src="image.src + 
     '?imageView2/1/w/600/h/600/q/75|imageslim'"/>
     </div>
@@ -24,8 +15,6 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Cornography',
-      ifShowInfo: false,
       images: [
         {id: 1, src: 'http://pic.ustb.wang/onref/1.JPG'},
         {id: 2, src: 'http://pic.ustb.wang/onref/2.JPG'},
@@ -47,35 +36,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  margin: 0;
-  padding: 0;
-}
-
-.title {
-  font-size: 46px;
-}
-
-.subTitle {
-  font-family: 'roboto';
-  margin-top: 10px;
-  font-size: 18px;
-  font-weight: 300;
-  line-height: 32px;
-}
-
-.subTitle span {
-  text-decoration: underline;
-}
-
-canvas {
-  margin-top: 20px;
-  background-color: #333;
-}
-
-a {
-  color: #2c3e50;
-}
 
 img {
   margin-top: 40px;
@@ -89,4 +49,5 @@ img {
   flex-direction: row;
   overflow-x: scroll;
 }
+
 </style>
